3bc2eb1378a046de96ec89b075c9c6ff2ff4ac4b
Erik Corry <erikcorry@google.com>
2016-12-07 22:30:13 +1100

    code

--- grut.dart
+++ grut.dart
@@ -1,9 +1,17 @@
-class Ast {
+abstract class Ast {
   static int ctr = 0;
   String name = "f${ctr++}";
 
   void dump() {
     print('"$name" [label="$this"];');
+  }
+  void gen(String successor);
+  String get linkage => name == "f0" ? "external" : "internal";
+  void forward(String to) {
+    print("define $linkage i32 @$name(i8* %s) {");
+    print("  %result = call i32 @$to(i8* %s)");
+    print("  ret i32 %result");
+    print("}");
   }
 }
 
@@ -25,6 +33,28 @@ class Literal extends Ast {
   String str;
 
   String toString() => str;
+  void gen(String successor) {
+    print("define $linkage i32 @$name(i8* %s) {");
+    // char c = *s
+    print("  %c = load i8, i8* %s, align 1");
+    // bool comparison = c == ascii_code_of_literal
+    print("  %comparison = icmp eq i8 %c, ${str.codeUnitAt(0)}");
+    // if comparison goto matched else goto got_result;
+    print("  br i1 %comparison, label %matched, label %got_result");
+    print("matched:");
+    // char* next = s + 1;
+    print("  %next = getelementptr i8, i8* %s, i64 1");
+    // int succ_result = f42(next);
+    print("  %succ_result = call i32 @$successor(i8* %next)");
+    // goto got_result;
+    print("  br label %got_result");
+    print("got_result:");
+    // int result = phi(succ_result, 0);
+    print("  %result = phi i32 [ %succ_result, %matched ], [ 0, %0 ]");
+    // return result
+    print("  ret i32 %result");
+    print("}");
+  }
 }
 
 // A series of terms matched one after the other.
@@ -32,10 +62,18 @@ class Alternative extends BinaryAst{
   Alternative(Ast l, Ast r) : super(l, r);
 
   String toString() => "($l$r)";
+  void gen(String succ) {
+    forward(l.name);
+    l.gen(r.name);
+    r.gen(succ);
+  }
 }
 
 class EmptyAlternative extends Ast {
   String toString() => "";
+  void gen(String succ) {
+    forward(succ);
+  }
 }
 
 // A series of alternatives separated by '|'.
@@ -43,6 +81,21 @@ class Disjunction extends BinaryAst{
   Disjunction(Ast l, Ast r) : super(l, r);
 
   String toString() => "($l|$r)";
+  void gen(String succ) {
+    print('define $linkage i32 @$name(i8* %s) {');
+    print('  %left = call i32 @${l.name}(i8* %s)');
+    print('  %comparison = icmp eq i32 %left, 0');
+    print('  br i1 %comparison, label %left_failed, label %got_result');
+    print('left_failed:');
+    print('  %right = call i32 @${r.name}(i8* %s)');
+    print('  br label %got_result');
+    print('got_result:');
+    print('  %result = phi i32 [ %right, %left_failed ], [ 1, %0 ]');
+    print('  ret i32 %result');
+    print('}');
+    l.gen(succ);
+    r.gen(succ);
+  }
 }
 
 class Parser {
@@ -114,11 +167,16 @@ class Parser {
   }
 }
 
-int main() {
-  Parser parser = new Parser("ab?c");
+int main(List<String> args) {
+  Parser parser = new Parser("a(b|c)?d");
   Ast ast = parser.parse();
-  print("Digraph G {");
-  ast.dump();
-  print("}");
-  return 0;
+  if (args[0] == "dot") {
+    print("Digraph G {");
+    ast.dump();
+    print("}");
+  } else {
+    print("declare i32 @match(i8* %s)");
+    ast.gen("match");
+    return 0;
+  }
 }
