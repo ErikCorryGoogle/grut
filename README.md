a3d54d1e829ea03184437050d7725d59abc5cdf5
Erik Corry <erikcorry@google.com>
2016-12-16 22:53:12 +1100

    Implement '+' and other counted loops

--- README.md
+++ README.md
@@ -6,13 +6,13 @@ Yow! conference in Sydney 2016.  Try out with:
 make
 ./grut /usr/share/dict/words
 ```
-
 Features:
 * Regexp style is backtracking.
-* Supports *.()? and literals.
-* No support for []+{} or backslash escapes.
+* Supports *+.()?{} and literals.
+* No support for [] or backslash escapes.
 * No support for capturing brackets.
 
 Known issues:
 * Can't match a null character, and expects a null terminated string.
 * Doesn't avoid zero length matches in loops, so it can hang on zero length loop bodies like (a?)* (see note 4 in section 15.10.2.5 of https://www.ecma-international.org/ecma-262/5.1/ )
+* Counted loops inside loops won't count right.  Eg (ab+c)*
--- grut.cc
+++ grut.cc
@@ -3,19 +3,25 @@
 #include <sstream>
 #include <string>
 
-extern "C" int grut(const char* s);
+struct GrutState {
+  // TODO: We need to handle the correct sizing of this.
+  int captures_and_registers[32];
+};
 
-extern "C" int match(const char* s) {
+extern "C" int grut(GrutState* state, const char* s);
+
+extern "C" int match(GrutState* state, const char* s) {
   return 1;
 }
 
 int main(int argc, const char* const* argv) {
+  GrutState state;
   while (--argc) {
     const char* fn = *++argv;
     std::ifstream input(fn);
     std::string line;
     while (std::getline(input, line)) {
-      if (grut(line.c_str())) {
+      if (grut(&state, line.c_str())) {
 	std::cout << line << "\n";
       }
     }
