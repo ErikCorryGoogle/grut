fe5cc21809d3d77f4a104bdd2d69e41f35784f07
Erik Corry <erikcorry@google.com>
2017-01-21 03:46:10 +0100

    Use guard page in tests to reveal out of bounds reads or writes

--- tests/grutdump.cc
+++ tests/grutdump.cc
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
 
 struct GrutState {
   char* start_of_string;
@@ -10,23 +11,46 @@ struct GrutState {
 
 extern "C" int grut(GrutState* state, const char* s);
 
+static const intptr_t kBufSize = 4 << 20;
+static const intptr_t kMargin = 64 << 10;  // 64k.
+
+static char tmp[kBufSize];
+
 static void dump(GrutState* state, int capturePairs) {
-  *state->captures_and_registers[1] = '\0';
-  printf("«%s»\n", state->captures_and_registers[0]);
+  int len = state->captures_and_registers[1] - state->captures_and_registers[0];
+  memcpy(tmp, state->captures_and_registers[0], len);
+  tmp[len] = '\0';
+  printf("«%s»\n", tmp);
   for (int i = 2; i < capturePairs; i += 2) {
     if (state->captures_and_registers[i] == nullptr) {
       printf("%d: «null»\n", i >> 1);
     } else {
+      char* start = state->captures_and_registers[i];
       char* end = state->captures_and_registers[i + 1];
-      char saved = *end;
-      *end = '\0';
-      printf("%d: «%s»\n", i >> 1, state->captures_and_registers[i]);
-      *end = saved;
+      memcpy(tmp, start, end - start);
+      tmp[end - start] = '\0';
+      printf("%d: «%s»\n", i >> 1, tmp);
     }
   }
 }
 
 int main(int argc, const char* const* argv) {
+  void* arena = mmap(nullptr, kBufSize, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+  if (arena == MAP_FAILED) {
+    perror("mmap");
+    exit(8);
+  }
+  char* start = reinterpret_cast<char*>(arena) + kMargin;
+  char* end = reinterpret_cast<char*>(arena) + kBufSize - kMargin;
+  if (mprotect(arena, kMargin, PROT_NONE)) {
+    perror("mprotect1");
+    exit(8);
+  }
+  if (mprotect(end, kMargin, PROT_NONE)) {
+    perror("mprotect2");
+    exit(8);
+  }
+
   bool multiline = false;
   if (argc >= 1 && !strcmp(argv[1], "-m")) {
     multiline = true;
@@ -44,10 +68,9 @@ int main(int argc, const char* const* argv) {
     perror(fn);
     exit(1);
   }
-#define BUF_SIZ (4 << 20)
-  static char buffer[BUF_SIZ];
-  size_t bytes = fread(buffer, 1, BUF_SIZ, fp);
-  if (bytes == BUF_SIZ) {
+  static char buffer[kBufSize];
+  size_t bytes = fread(buffer, 1, kBufSize, fp);
+  if (bytes == kBufSize) {
     fprintf(stderr, "Input too large: %s\n", fn);
     exit(2);
   }
@@ -62,23 +85,53 @@ int main(int argc, const char* const* argv) {
     // Remove one trailing newline from the input because many editors like
     // to add a newline to the last line,
     if (buffer[bytes - 1] == '\n') buffer[bytes - 1] = '\0';
+    bytes--;
   }
   int capturePairs = grut(nullptr, nullptr);
   if (multiline) {
     while (true) {
+      bool done = false;
       char* newline = const_cast<char*>(strchr(input, '\n'));
-      if (newline != nullptr) *newline = '\0';
-      if (grut(&state, input)) {
+      if (newline == nullptr) {
+	done = true;
+	newline = const_cast<char*>(input + strlen(input));
+      } else {
+	if (*newline != '\0') *newline = '\0';
+      }
+      int len = newline + 1 - input;
+      memcpy(start, input, len);
+      memcpy(end - len, input, len);
+      if (mprotect(start, kBufSize - 2 * kMargin, PROT_READ)) {
+	perror("mprotect3");
+	exit(42);
+      }
+      if (grut(&state, start)) {
 	dump(&state, capturePairs);
       } else {
 	printf("\n");
       }
-      if (newline == nullptr) break;
+      grut(&state, end - len);  // Check if we hit the guard.
+      if (mprotect(start, kBufSize - 2 * kMargin, PROT_READ | PROT_WRITE)) {
+	perror("mprotect4");
+	exit(103);
+      }
+      if (done) break;
       input = newline + 1;
     }
   } else {
-    if (grut(&state, input)) {
+    memcpy(start, input, bytes + 1);
+    memcpy(end - bytes - 1, input, bytes + 1);
+    if (mprotect(start, kBufSize - 2 * kMargin, PROT_READ)) {
+      perror("mprotect3");
+      exit(42);
+    }
+    if (grut(&state, start)) {
       dump(&state, capturePairs);
+    }
+    grut(&state, end - bytes - 1);  // Check if we hit the guard.
+    if (mprotect(start, kBufSize - 2 * kMargin, PROT_READ | PROT_WRITE)) {
+      perror("mprotect4");
+      exit(103);
     }
   }
   return 0;
